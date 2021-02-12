397a4b959860a627863efb5f7d8518d486f560e4
Devang Mehta (C) <devangm@splunk.com>
2021-02-12 19:10:55 +0530

    test: update sc4s_sync.sh for version value

--- sc4s_sync.sh
+++ sc4s_sync.sh
@@ -18,8 +18,14 @@ else
     exit 1
 fi
 
-current_tag=$(cat SC4S_matrix.conf | grep -oP '(?<=VERSION=).*')
+current_tag=$(cat sc4s_matrix.conf | grep -oP 'VERSION\s*=\s*([0-9\.]+)')
 echo $current_tag
+re='VERSION\s*=\s*([0-9.]+)'
+if [[ $current_tag =~ $re ]]
+then
+    current_tag_value=${BASH_REMATCH[1]}
+fi
+echo $current_tag_value
 
 latest_tag=$(curl -s https://api.github.com/repos/splunk/splunk-connect-for-syslog/releases/latest | grep "tag_name" | cut -d : -f 2)
 new_value=$(echo $latest_tag | rev | cut -c3- | rev)
